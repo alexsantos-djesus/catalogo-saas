a0f38b6d8920bbe1923489ee40d68e1cb5d1628f
alexsantos-djesus <alexsantos.djesus@gmail.com>
2025-12-27 16:09:54 -0300

    fix: correct auth helper using nextauth v5 api

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,4 @@
-import { auth as nextAuth } from "next-auth";
+import NextAuth from "next-auth";
 import { authConfig } from "@/lib/auth-config";
 
-export const auth = nextAuth(authConfig);
+export const { auth } = NextAuth(authConfig);
